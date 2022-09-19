f1d5bf279155d77eb381881b6b79b4e5d33c3891
SpencerNelsonUW <spencernelsonn@gmail.com>
2022-09-18 18:04:33 -0700

    adding more to the server to get and post

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 const express = require('express');
 const path = require('path');
 const uuid = require('uuid');
-const notes = require('./db/db.json');
+
+const database = require('./db/db.json');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -25,6 +26,23 @@ app.get('/', (req, res) =>
   res.render(path.join(__dirname, '/public/notes.html'))
 );
 
+//
+app.route('/api/notes').get(function(req, res){
+  res.json(database);
+});
+
+app.route("/api/notes").post(function(req, res){
+  let jsonFile = path.join(__dirname, '/db/db.json');
+  let newNote = req.body;
+  database.push(newNote)
+  fs.writeFile(jsonFile, JSON.stringify(database), function (err) {
+    if (err){
+      return console.log(err);
+    }
+    console.log("note saved succesfully")
+  })
+  res.json(newNote);
+})
 
 
 //listening to set up the server.
