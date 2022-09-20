62e9036cc6b4271588629f6d9b63432b9a57c13a
SpencerNelsonUW <spencernelsonn@gmail.com>
2022-09-19 18:28:23 -0700

    fixed the server to be able to add notes

--- server.js
+++ server.js
@@ -1,36 +1,47 @@
 //dependencies
 const fs = require('fs'); 
+const path = require("path");
 const express = require('express');
-const path = require('path');
-const uuid = require('uuid');
 
 const database = require('./db/db.json');
 
+//setting up express app
 const app = express();
+//setting up the port to use
 const PORT = process.env.PORT || 3001;
 
-//linking
+//use data held within the public directory
 app.use(express.static('public'));
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-//HTML ROUTES
+//ROUTES
 // GET route for homepage, this should open initially
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 )
 //GET route for notes
-app.get('/', (req, res) =>
-  res.render(path.join(__dirname, '/public/notes.html'))
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 )
 
-//
+
+//GET route for database
+//route to read the `db.json` file and return all saved notes as JSON.
+app.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/db/db.json'));
+});
+
+
+
+//GET NOTES
 app.route('/api/notes').get(function(req, res){
   res.json(database);
 })
 
+//Push new notes to database
 app.route("/api/notes").post(function(req, res){
   let jsonFile = path.join(__dirname, '/db/db.json');
   let newNote = req.body;
